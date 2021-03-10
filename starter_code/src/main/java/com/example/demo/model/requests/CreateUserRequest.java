cda1d7e1e2df5f9471c03168dec8960c7195cf22
qmehdi <qmehdi110@gmail.com>
2021-03-10 11:34:13 -0600

    Added password and verifyPassword fields in the request as part of createUser

--- starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -7,11 +7,34 @@ public class CreateUserRequest {
 	@JsonProperty
 	private String username;
 
+	@JsonProperty
+	private String password;
+
+	@JsonProperty
+	private String verifyPassword;
+
+
 	public String getUsername() {
 		return username;
 	}
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getVerifyPassword() {
+		return verifyPassword;
+	}
+
+	public void setVerifyPassword(String verifyPassword) {
+		this.verifyPassword = verifyPassword;
 	}
 }
